bb9d34dda6513dc6cd6b5c5076ec36e14df4e345
Ali Rasheed <alirasheed@Alis-MacBook-Pro-2.local>
2022-05-31 14:22:17 -0500

    ios added

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'airtesting',
+  appName: 'brotherskeptsecret',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
